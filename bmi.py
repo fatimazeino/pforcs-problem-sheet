af30163661ad579b8c59278dadc7736745fd5372
unknown <fatemazeno.fz@gmail.com>
2021-03-07 13:03:14 +0000

    task week 02

---
+++ bmi.py
@@ -0,0 +1,16 @@
+# Body Mass Index (BMI).
+# take height in cm 
+# convert height to m
+# squraed height 
+# take weight
+# weight divided by  height in squared.
+# take two numbers after decimal point
+#
+height = float(input("Enter yor height in centimaters "))
+height_maters = height/100
+height_squared = height_maters*height_maters
+weight = float(input("Enter your weight in kilograms "))
+bmi = float(weight / height_squared)
+txt = "your Body Mass Index is "
+bmi_formatted = "{:.2f}".format(bmi)
+print(txt + bmi_formatted)
