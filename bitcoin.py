5ff4c44f7c52fba916c2f90c6d8d88a18b955afb
unknown <fatemazeno.fz@gmail.com>
2021-03-07 13:04:51 +0000

    task week 03

---
+++ bitcoin.py
@@ -0,0 +1,20 @@
+import requests
+url="https://api.coindesk.com/v1/bpi/currentprice.json"
+returnedData=requests.get(url)
+bitcoindict=returnedData.json()
+print( "Today bitcoin price in " + str(bitcoindict['bpi']['USD']['description']) + " is : " + str(bitcoindict['bpi']['USD']['rate_float']))
+
+
+''' {"time":
+{"updated":"Feb 14, 2021 00:16:00 UTC","updatedISO":"2021-02-14T00:16:00+00:00","updateduk":"Feb 14, 2021 at 00:16 GMT"
+},
+"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","chartName":"Bitcoin",
+"bpi":
+{"USD":
+{"code":"USD","symbol":"&#36;","rate":"47,627.3615","description":"United States Dollar","rate_float":47627.3615},
+"GBP":
+{"code":"GBP","symbol":"&pound;","rate":"34,407.8634","description":"British Pound Sterling","rate_float":34407.8634}
+,"EUR"
+{"code":"EUR","symbol":"&euro;","rate":"39,294.9070","description":"Euro","rate_float":39294.907}
+}
+} '''
