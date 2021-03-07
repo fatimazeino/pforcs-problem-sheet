801e550d2e8298b575b56f4c7c171a7d5f3d15c7
unknown <fatemazeno.fz@gmail.com>
2021-03-07 13:09:41 +0000

    task week 02

---
+++ sendemail.py
@@ -0,0 +1,16 @@
+# send an email via gmail
+
+import smtplib
+
+username = 'fatimaprog2021@gmail.com'
+password = 'fatima@2021'
+toemail = 'fatemazeno.fz@gmail.com'
+massege = 'hi this an test email'
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+
+server.login(username,password)
+server.sendmail(username, toemail, massege)
+
+print ("Done")
